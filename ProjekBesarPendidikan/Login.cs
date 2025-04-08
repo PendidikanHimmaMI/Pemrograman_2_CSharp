196b04340afed5a20cc65547fddeab33915736fd
virya <0320230119@polytechnic.astra.ac.id>
2025-04-08 23:27:43 +0700

    p

---
+++ ProjekBesarPendidikan/Login.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjekBesarPendidikan
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            this.WindowState = FormWindowState.Maximized;
+            InitializeComponent();
+        }
+        private void btnLogin_Click(object sender, EventArgs e) {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (username == "admin" && password == "1234") {
+                MessageBox.Show("Login successful!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                // Proceed to main form here
+            } else {
+                MessageBox.Show("Invalid username or password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+    }
+}
